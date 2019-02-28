e9e39d126712280ce87ed14cc7ef476c3a9d658f
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-28 20:09:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,9 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   data.each do |key, value_hash|
+    binding.pry
     key.each do |tags, names|
-      binding.pry
+    
     end
   end# write your code here!
 end
