ea49e0cb532f12d23568a303dc3a67f735dcc68e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-28 20:17:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,15 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash ={}
+  i = 0
   data.each do |key, value_hash|
     value_hash.each do |tags, names|
-      new_hash[names[0]] = key
+      new_hash[names[i]] = key
+      if(new_hash.include? name[i])
+        new_hash[name[i+1]] = key
 
 binding.pry
+      end
     end
   end# write your code here!
 end
