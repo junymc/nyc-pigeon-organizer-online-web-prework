316a6eddcb21ae2cf616a5ab252653fd3b518cdb
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-28 20:34:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value_hash|
     value_hash.each do |tags, names|
       names.each do |name|
-        "#{name} => {key}"
+        if(new_hash.has_key?)
+        new_hash[name][key] = tags
       end
     end
   end
