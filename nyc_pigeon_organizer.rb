d9b58471e6f483e3c1814e02d0a705e1a090e033
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-28 20:50:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,8 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
+  # get the names as first keys of new_hash
+  # add the first key from data to new_hash as a key in value_hash
+  # add the key of value_hash from data to new_hash as a value in the value_hash
   new_hash ={}
   data.each do |key, value_hash|
     value_hash.each do |tags, names|
